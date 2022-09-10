531653a4aea49c5e80e7155c8ecc5b48c1958783
ababoi4 <105204537+ababoi4@users.noreply.github.com>
2022-09-10 20:16:17 +0300

    Make CARMUSC

---
+++ Верстка/js/script.js
@@ -0,0 +1,36 @@
+$('.slider').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+$(document).ready(function() {
+  $('.header__burger').click(function (event) {
+    $('.header__burger,.header').toggleClass('active');
+    $('.nav-menu__list').slideToggle(600);
+  });
+});
